5bfad14ed5eb0c935b44d5ad7d093daaee5ea4ad
Ubuntu <ubuntu@ip-172-31-34-94.eu-west-3.compute.internal>
2023-06-21 18:27:56 +0000

    qr generator script

--- public/qr_generator.py
+++ public/qr_generator.py
@@ -4,5 +4,5 @@ qr=qrcode.QRCode(border=5, box_size=11)
 qr.add_data('https://tweetnfts.xyz')
 qr.make(fit=True)
 img = qr.make_image(fill_color="white", back_color="gray")
-img.save("qr_tweetnftsWebsite")
+img.save("qr_tweetnftsWebsite.png")
 
